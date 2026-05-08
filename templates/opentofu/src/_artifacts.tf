074a5f8ff9e20a957bae1f8c0b2f6811f2dd979d
Cory O'Daniel <cory@coryodaniel.com>
2026-05-07 21:25:48 -0700

    Update _artifacts.tf

--- templates/opentofu/src/_artifacts.tf
+++ templates/opentofu/src/_artifacts.tf
@@ -6,13 +6,8 @@
 #   provider_resource_id = aws_instance.main.arn  # or other unique resource ID
 #   name                 = "My Artifact (${var.md_metadata.name_prefix})"
 #   artifact = jsonencode({
-#     data = {
-#       # Fields matching your artifact definition schema
-#       endpoint = aws_instance.main.public_ip
-#       port     = 443
-#     }
-#     specs = {
-#       # Optional specs for policy/compliance
-#     }
+#     # Fields matching your artifact definition schema
+#     endpoint = aws_instance.main.public_ip
+#     port     = 443
 #   })
 # }
